700b6191aac8fc44e82130a0446cd28cd27cbfeb
LikeToAccess <375916737591675+LikeToAccess@users.noreply.github.com>
2022-04-28 10:26:45 -0500

    Code works!

--- main.py
+++ main.py
@@ -13,23 +13,113 @@
 import platform
 import sys
 import os
+# import time
+import json
+import tqdm
+import instaloader
+from settings import *
 
 
 OS = platform.system()
 profiles = sys.argv[1:]
+il = instaloader.Instaloader()
 
+
+def remove_file(filename):
+	try:
+		os.remove(filename)
+		return True
+	except OSError:
+		return False
+
+def read_file(filename, encoding="utf8"):
+	if not os.path.exists(filename): return filename
+	with open(filename, "r", encoding=encoding) as file:
+		data = file.read()
+
+	return data
+
+def write_file(filename, data, encoding="utf8"):
+	with open(filename, "w", encoding=encoding) as file:
+		file.write(data)
+
+def read_json_file(filename, encoding="utf8"):
+	if not os.path.exists(filename): return []
+	with open(filename, "r", encoding=encoding) as file:
+		data = json.load(file)
+
+	return data
+
+def write_json_file(filename, data, encoding="utf8"):
+	with open(filename, "w", encoding=encoding) as file:
+		json.dump(data, file, indent=4, sort_keys=True)
+
+	return json.dumps(data, indent=4, sort_keys=True)
+
+def append_json_file(filename, data, encoding="utf8"):
+	existing_data = read_json_file(filename, encoding=encoding)
+	existing_data.append(data)
+	write_json_file(filename, existing_data, encoding=encoding)
+
+def reformat_json_to_js(filename, encoding="utf8"):
+	json_text = read_file(filename)
+	js_text = "var obj = \n" + json_text + ";"
+	write_file(filename + ".js", js_text, encoding=encoding)
+
+def finalize(filename):
+	reformat_json_to_js(filename)
+	print(read_file(filename))
+
+def login_to_instagram():
+	try:
+		il.login(USERNAME, PASSWORD)
+		return True
+	except instaloader.exceptions.ConnectionException:
+		return False
+
+def download_posts(profile):
+	profile = instaloader.Profile.from_username(il.context, profile)
+	posts = profile.get_posts()
+	# il.download_profiles([profile], profile_pic=False)#, post_filter="congrats")
+	for post in posts:
+		print(post.date_utc)
+		print(post.caption)
+		il.download_post(post, target=profile.username)
+
+	return profile
 
 def main():
 	if OS == "Windows":
 		ffmpeg_binary = "ffmpeg.exe"
-	else: # Works for MacOS and Linux
+	else:  # Works for MacOS and Linux
 		ffmpeg_binary = "./ffmpeg"
 
+	profile = None
 	for profile in profiles:
 		print(profile)
-		os.system(f"instaloader --fast-update {profile}")
+		# while not login_to_instagram():
+		# 	time.sleep(3)
+		# download_posts(profile)
+		remove_file("2022-03-19_06-15-57_UTC.png")
+		remove_file("2022-03-19_06-15-57_UTC.webp")
 
+		profile_images = []
+		for file in tqdm.tqdm(os.listdir(profile)):
+			profile_image = {}
+			if file.endswith(".webp"):
+				os.system(f"{ffmpeg_binary} -i {profile}/{file} {profile}/{file.replace('.webp','.png')} -y -hide_banner -loglevel error")
 
+				profile_image[file.replace(".webp", "")] = {
+					"image_filename":   file.replace(".webp", ".png"),
+					"text":   read_file(profile+"/"+file.replace(".webp", ".txt")).strip("\n"),
+				}
+
+				profile_images.append(profile_image)
+
+		remove_file(profile+".json")
+		append_json_file(profile+".json", profile_images)
+
+	finalize(profile+".json")
 
 
 if __name__ == "__main__":
